c51b066e0f10bcbb77b6671415bdd5dc259f74a5
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-26 10:35:57 +0100

    Add files via upload

---
+++ vues/creerGestionnaire.php
@@ -0,0 +1,62 @@
+<div class="haut0"><p>Gestion des droits d'accès</p></div>
+
+<div class="ensemble">
+    <div class="gauche">
+        <div class="haut">
+            <p>Créer un compte Gestionnaire</p>
+        </div>
+        <form method="post">       <!--action="traitement.php"-->
+            <fieldset>
+                <div>
+                    <label for="pseudo">Nom d'utilisateur<br/></label>
+                    <input type="text" name="pseudo" id="pseudo" autofocus required/>
+                    <span class="commentaire"><br/>Un mot de passe aléatoire sera envoyé au gestionnaire <br/><br/></span>
+                </div>
+                <div>
+                    <label for="mailGestionnaire">Adresse courriel du gestionnaire<br/></label>
+                    <input type="email" name="email" id="mailGestionnaire" required/>
+                </div>
+                <div>
+
+                    <label for="passAdmin"><br/>Votre mot de passe<br/></label>
+                    <input type="password" name="pass" id="passAdmin" required/>
+                </div>
+
+            </fieldset>
+            <input type="submit" value="Confirmer"/>
+        </form>
+
+    </div>
+
+
+    <div class="droit">
+        <div class="haut">
+            <p>Supprimer un compte gestionnaire</p>
+        </div>
+        <form method="post">       <!--action="traitement.php"-->
+            <fieldset>
+                <div>
+                    <label for="pseudo">Nom d'utilisateur<br/></label>
+                    <input type="text" name="pseudo" class="pseudo" autofocus required/>
+                    <!--ici on n'a pas id="pseudo" mais class="pseudo", créera un potentiel bug-->
+                </div>
+                <div>
+                    <label for="nom">Nom du gestionnaire<br/></label>
+                    <input type="text" name="nom" id="nom" required/>
+                </div>
+                <div>
+                    <label for="prenom">Prénom du gestionnaire<br/></label>
+                    <input type="text" name="prenom" id="prenom" required/>
+                </div>
+                <div>
+                    <label for="passAdmin">Votre mot de passe<br/></label>
+                    <input type="password" name="pass" class="passAdmin" required/>
+                    <!--ici on n'a pas id="nom" mais un class, créera un potentiel bug-->
+                </div>
+
+            </fieldset>
+            <input type="submit" value="Confirmer"/>
+        </form>
+
+    </div>
+</div>
