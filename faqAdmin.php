a9a6cc636d97ae541bee7a127e4f969411b4e679
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-11-29 22:10:57 +0100

    Add files via upload
    ajouter ci-dessous dans controleurs/admin.php
       
     case 'faqAdmin':
            $title="FAQ";
            $faq="faq";  //nom de la table faq de la bdd
            $donneesfaq = recupereTous($bdd,$faq);
            $vue= "faqAdmin";
            $css="CSSfaq";
            break;
    
    et ajouter ci-dessous dans modele/requete.utilisateurs.php
    
    function ajouteQuestion(PDO $bdd, $question)
    {
        $query = "INSERT INTO faq (idQ, contenuQuestion) VALUES ((SELECT MAX(idQ) FROM faq) + 1, $question)";
        $donnees = $bdd->prepare($query);
        $donnees->execute();
    }

---
+++ faqAdmin.php
@@ -0,0 +1,15 @@
+<?php include("faq.php");?>
+
+<!--ajouter une question-->
+
+<form method="POST" action="">
+    <label for="ajoutQuestion"><br/>Vous pouvez ajouter une question ici ... <br/></label>
+    <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea><br/>
+    <input type="submit" value="SUBMIT">
+</form>
+<?php if(isset($_POST['ajoutQuestion'])){
+    echo $_POST['ajoutQuestion'];
+    $question = $_POST['ajoutQuestion'];
+    ajouteQuestion($bdd,htmlspecialchars($question));
+}
+?>
