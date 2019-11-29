10a5ac86e43413996c96fe96dfaa4e0b8ed5179b
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-29 15:32:11 +0100

    Add files via upload

--- controleurs/fonctions.php
+++ controleurs/fonctions.php
@@ -55,3 +55,16 @@ function crypterMdp($password) {
     return password_hash($password, PASSWORD_BCRYPT);
 }
 
+function chaine_aleatoire($nb_car, $chaine = 'azertyuiopqsdfghjklmwxcvbn123456789')
+{
+    $nb_lettres = strlen($chaine) - 1;
+    $generation = '';
+    for($i=0; $i < $nb_car; $i++)
+    {
+        $pos = mt_rand(0, $nb_lettres);
+        $car = $chaine[$pos];
+        $generation .= $car;
+    }
+    return $generation;
+}
+
