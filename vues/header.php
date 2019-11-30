2c90c3ee173624a2b71d2db6d4f4c52b78eca97c
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-30 21:04:22 +0100

    Update header.php

--- vues/header.php
+++ vues/header.php
@@ -3,6 +3,15 @@
     <link rel="stylesheet" type="text/css" href="vues/<?php echo $css?>.css">
     <link rel="stylesheet" type="text/css" href="vues/CSSheader.css">
     <meta charset="utf-8">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#dialog" ).dialog();
+        } );
+    </script>
     <title><?php echo $title;?></title>
 </head>
 <body>
@@ -21,3 +30,8 @@
         </ul>
     </nav>
 </header>
+<?php if($alerte){  ?>
+    <?php echo '<div id="dialog" title="Information">
+    <p>'.$alerte.'</p>
+</div>'; }?>
+
