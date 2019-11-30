992fa484b44245e7b58527fe356d87e7d4727fbf
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-30 20:45:11 +0100

    Update faqAdmin.php

--- vues/faqAdmin.php
+++ vues/faqAdmin.php
@@ -1,22 +1,6 @@
 <div id="vide"></div>
-<!--ajouter une question-->
-<div id="faq">
-    <h1>FAQ</h1>
-    <div id="parametrer">
-        <form method="POST" action="">
-            <div class="label">
-                <label for="ajoutQuestion">Veuillez saisir la question </label>
-                <label for="ajoutReponse">Veuillez saisir la réponse </label>
-            </div>
-            <div class="input">
-                <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea>
-                <textarea name="ajoutReponse" class="ajoutReponse" rows="5" cols="60"></textarea>
-            </div>
-            <input type="submit" value="SUBMIT">
-        </form>
-    </div>
-</div>
 <div id="contenuFaq">
+    <h1>FAQ</h1>
     <?php
     foreach ($donneesfaq as $element) { ?>
         <!--bloc questionReponse-->
@@ -32,6 +16,22 @@
 </div>
 <?php if($alerte){  ?>
     <?php echo '<SCRIPT language="Javascript">alert(\''.$alerte.'\');</SCRIPT>'; }?>
+<!--ajouter une question-->
+<div id="faq">
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="label">
+                <label for="ajoutQuestion">Veuillez saisir la question </label>
+                <label for="ajoutReponse">Veuillez saisir la réponse </label>
+            </div>
+            <div class="input">
+                <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea>
+                <textarea name="ajoutReponse" class="ajoutReponse" rows="5" cols="60"></textarea>
+            </div>
+            <input type="submit" value="SUBMIT">
+        </form>
+    </div>
+</div>
 <div id="vide"></div>
 <div id="vide"></div>
 
