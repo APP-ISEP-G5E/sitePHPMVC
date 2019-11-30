835b708157ff781c7cf6f4c55b403b1bd5aa35c5
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-30 12:38:29 +0100

    Add files via upload

--- vues/accueilAdmin.php
+++ vues/accueilAdmin.php
@@ -1,54 +1,36 @@
-<div id="vide" xmlns="http://www.w3.org/1999/html"></div>
-<a href="index.php?cible=admin&fonction=gestcandidat">Gestion candidat</a>
-<a href="index.php?cible=admin&fonction=gestgestionnaire">Gestion gestionnaire</a>
-<a href="index.php?cible=admin&fonction=listeUtilisateurs">Lister utilisateurs</a>
-<div class="box">
-    <div class="box1">
-        <h1>Alerte : </h1>
-            <ul>
-                <li><a href="">Alerte capteur température</a></li>
-                <li><a href="">Alerte capteur rythme cardiaque</a></li>
-                <li><a href="">Alerte capteur sonore</a></li>
-            </ul>
-        </div>
+<div id="vide">
 
-    <div class="box2">
-        <h1>Gestion droits d'accès : </h1>
-            <ul>
-                <li><a href="index.php?cible=admin&fonction=gestcandidat">Créer/supprimer un compte candidat</a></li>
-                <li><a href="index.php?cible=admin&fonction=gestgestionnaire">Créer/supprimer un compte gestionnaire</a></li>
-                <li><a href="">Modifier un mot de passe</a></li>
-            </ul>
-    </div>
 </div>
-
-<div class="box">
-    <div class="box1">
-        <h1>Gestion capteurs : </h1>
-            <ul>
-                <li><a href="">Modifier les seuils des capteurs</a></li>
-                <li><a href="">Ajouter un capteur</a></li>
-                <li><a href="">Enlever un capteur</a></li>
-            </ul>
-    </div>
-
-
-    <div class="box2">
-        <h1><a href="">Données des candidats</a></h1>
-    </div>
-</div>
-
-<div class="box">
-    <div class="box1">
-        <h1>Gestion actionneurs : </h1>
-            <ul>
-                <li><a href="">Modifier les seuils des actionneurs</a></li>
-                <li><a href="">Ajouter un Actionneur</a></li>
-                <li><a href="">Enlever un Actionneur</a></li>
-            </ul>
-    </div>
-
-    <div class="box2">
-        <h1><a href="">Gérer la page FAQ</a></h1>
-    </div>
-</div>
+<nav id="naviguation">
+    <div id="margin"></div>
+    <h3 class="titre"><span class="ruban">ALERTE</span></h3>
+    <ul>
+        <li><a class="sous-titre" href="#">Alerte capteur température</a></li>
+        <div class=""></div>
+        <li><a class="sous-titre" href="#">Alerte capteur rythme cardiaque</a></li>
+        <div></div>
+        <li><a class="sous-titre" href="#">Alerte capteur sonore</a></li>
+        <div></div>
+    </ul>
+    <h3 class="titre"><span class="ruban">GESTION DES ACCES</span></h3>
+    <ul>
+        <li><a class="sous-titre" href="index.php?cible=admin&fonction=gestcandidat">Créer un candidat</a></li>
+        <li><a class="sous-titre" href="index.php?cible=admin&fonction=gestgestionnaire">Créer un gestionnaire</a></li>
+        <li><a class="sous-titre" href="#">Modifier un mot de passe</a></li>
+    </ul>
+    <h3 class="titre"><span class="ruban">GESTION CAPTEURS</span></h3>
+    <ul>
+        <li><a class="sous-titre" href="#">Modifier les seuils</a></li>
+        <li><a class="sous-titre" href="#">Ajouter un capteur</a></li>
+        <li><a class="sous-titre" href="#">Enlever un capteur</a></li>
+    </ul>
+    <h3 class="titre" href=""><span class="ruban">RESULTATS CANDIDATS</span></h3>
+    <h3 class="titre"><span class="ruban">GESTION ACTIONNEURS</span></h3>
+    <ul>
+        <li><a class="sous-titre" href="#">Modifier les seuils</a></li>
+        <li><a class="sous-titre" href="#">Ajouter un Actionneur</a></li>
+        <li><a class="sous-titre" href="#">Enlever un Actionneur</a></li>
+    </ul>
+    <h3 class="titre"><span class="ruban">GERER LA FAQ</span></h3>
+    <div id="margin"></div>
+</nav>
