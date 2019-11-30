e71d640df8678e9325fd0b96ec7a6db9f88cfed2
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-30 19:29:35 +0100

    Add files via upload

--- vues/faqAdmin.php
+++ vues/faqAdmin.php
@@ -1,30 +1,22 @@
 <?php include("faq.php");?>
-
 <!--ajouter une question-->
-<div class="parametrer">
-    <div>
-    <form method="POST" action="">
-        <label for="ajoutQuestion"><br/>Vous pouvez ajouter une question ici : <br/></label>
-        <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea><br/>
-        <input type="submit" value="SUBMIT">
-    </form>
+<div id="faq">
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="label">
+                <label for="ajoutQuestion">Veuillez saisir la question </label>
+                <label for="ajoutReponse">Veuillez saisir la réponse </label>
+            </div>
+            <div class="input">
+                <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea>
+                <textarea name="ajoutReponse" class="ajoutReponse" rows="5" cols="60"></textarea>
+            </div>
+            <input type="submit" value="SUBMIT">
+        </form>
     </div>
-    <?php if(isset($_POST['ajoutQuestion']) && $_POST['ajoutQuestion'] !=''){
-        $question = htmlspecialchars($_POST['ajoutQuestion']);
-        ajouteQuestion($bdd,$question);
-    }
-    ?>
-    <div>
-    <form method="POST" action="">
-        <label for="ajoutReponse"><br/>Vous pouvez ajouter une réponse ici : <br/></label>
-        <input type="number"  name="numeroQuestion" min="1" placeholder="Numéro de la question" required/><br/>
-        <textarea name="ajoutReponse" class="ajoutReponse" rows="5" cols="60"></textarea><br/>
-        <input type="submit" value="SUBMIT">
-    </form>
-    </div>
-    <?php if(isset($_POST['ajoutReponse']) && $_POST['ajoutReponse'] !='' && isset($_POST['numeroQuestion'])){
-        $reponse= htmlspecialchars($_POST['ajoutReponse']);
-        modifierReponse($bdd,$_POST['numeroQuestion'],$reponse);
-    }
-    ?>
 </div>
+<?php if($alerte){  ?>
+    <?php echo '<SCRIPT language="Javascript">alert(\''.$alerte.'\');</SCRIPT>'; }?>
+<div id="vide"></div>
+<div id="vide"></div>
+
