507c9fa0ee1e06b3eca15316841a41ab03e56e8c
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-11-30 13:28:15 +0100

    Add files via upload
    mettre ça dans controleurs/admin.php   
     case 'faqAdmin':
            $title="FAQ";
            $faq="faq";  //nom de la table faq de la bdd
            $vue= "faqAdmin";
            $css="CSSfaq";
            $donneesfaq = recupereTous($bdd,$faq);
    
            break;
    
    et ci-dessous dans modele/requete.utilisateurs.php
    
    function ajouteQuestion(PDO $bdd, $question)
    {
        $recupereMax = $bdd->query('SELECT MAX(idQ) as max FROM faq');
        $idQMax = $recupereMax->fetch();      //fetch() renvoie la première ligne récupérée sous un array
        ++$idQMax['max'];     //'max' vient de 'as max' dans la requete recupereMax
        //$recupereMax->closeCursor();
        $reqq = $bdd->prepare('INSERT INTO faq(idQ,contenuQuestion) VALUES(:idQNext,:question)');
        $reqq->execute(array(':idQNext'=> $idQMax['max'] ,':question' => $question));
    }

--- vues/faqAdmin.php
+++ vues/faqAdmin.php
@@ -3,13 +3,13 @@
 <!--ajouter une question-->
 
 <form method="POST" action="">
-    <label for="ajoutQuestion"><br/>Vous pouvez ajouter une question ici ... <br/></label>
+    <label for="ajoutQuestion"><br/>Vous pouvez ajouter une question ici : <br/></label>
     <textarea name="ajoutQuestion" class="ajoutQuestion" rows="5" cols="60"></textarea><br/>
     <input type="submit" value="SUBMIT">
 </form>
-<?php if(isset($_POST['ajoutQuestion'])){
-    echo $_POST['ajoutQuestion'];
-    $question = $_POST['ajoutQuestion'];
+<?php if(isset($_POST['ajoutQuestion']) && $_POST['ajoutQuestion'] !=''){
+    $question = htmlspecialchars($_POST['ajoutQuestion']);
     ajouteQuestion($bdd,htmlspecialchars($question));
 }
 ?>
+
