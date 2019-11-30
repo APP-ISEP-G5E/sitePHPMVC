8c6e1e586c5e0c093253a222e6957a2d7d08aa2c
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-11-30 19:34:07 +0100

    Update faqAdmin.php

--- vues/faqAdmin.php
+++ vues/faqAdmin.php
@@ -1,4 +1,19 @@
-<?php include("faq.php");?>
+<div id="vide"></div>
+<div id="contenuFaq">
+    <h1>FAQ</h1>
+    <?php
+    foreach ($donneesfaq as $element) { ?>
+        <!--bloc questionReponse-->
+        <div class="blocQuestionReponse">
+            <div class="blocQuestion">
+                <?php echo $element['contenuQuestion']; ?>
+            </div>
+            <div class="blocReponse">
+                <?php echo $element['contenuReponse']; ?>
+            </div>
+        </div>
+    <?php } ?>
+</div>
 <!--ajouter une question-->
 <div id="faq">
     <div id="parametrer">
