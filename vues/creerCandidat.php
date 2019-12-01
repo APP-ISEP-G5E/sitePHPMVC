b1e17325bc60fc93522d0f052be2b796acf93ca9
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-12-01 14:11:02 +0100

    Add files via upload

--- vues/creerCandidat.php
+++ vues/creerCandidat.php
@@ -1,18 +1,15 @@
-<div id="vide">
-
-</div>
 <div id="contenuGestion">
     <div id="gestion">
         <h1 class="title_gest"><?php echo _CREER_CANDIDAT; ?></h1>
         <form method="POST" action="">
             <label for="username"><?php echo _SAISIR_CAND; ?></label>
-            <input type="text" id="username" name="username" placeholder="username">
+            <input type="text" name="new_user" placeholder="username">
 
             <label for="password"><?php echo _SAISIR_CANDIDAT; ?></label>
-            <input type="text" id="password" name="email" placeholder="email adress">
+            <input type="text" name="sup_email" placeholder="email adress">
 
             <label for="password"><?php echo _SAISIR_MDP; ?></label>
-            <input type="password" id="password" name="connex_mdp" placeholder="password">
+            <input type="password" name="verif_mdp" placeholder="password">
 
             <input type="submit" value="SUBMIT">
         </form>
@@ -22,13 +19,13 @@
         <h1 class="title_gest"><?php echo _SUP_CANDIDAT; ?></h1>
         <form method="POST" action="">
             <label for="username"><?php echo _SAISIR_CAND; ?></label>
-            <input type="text" id="username" name="connex_login" placeholder="username">
+            <input type="text" name="new_user" placeholder="username">
 
             <label for="password"><?php echo _SAISIR_CANDIDAT; ?></label>
-            <input type="text" id="password" name="connex_mdp" placeholder="email adress">
+            <input type="text" name="sup_email" placeholder="email adress">
 
             <label for="password"><?php echo _SAISIR_MDP; ?></label>
-            <input type="password" id="password" name="connex_mdp" placeholder="password">
+            <input type="password" name="verif_mdp" placeholder="password">
 
             <input type="submit" value="SUBMIT">
         </form>
--- vues/creerGestionnaire.php
+++ vues/creerGestionnaire.php
@@ -3,13 +3,13 @@
         <h1 class="title_gest"><?php echo _CREER_GESTIONNAIRE; ?></h1>
         <form method="POST" action="">
             <label for="username"><?php echo _SAISIR_GEST; ?></label>
-            <input type="text" id="username" name="connex_login" placeholder="username">
+            <input type="text" name="new_user" placeholder="username">
 
             <label for="password"><?php echo _SAISIR_GESTIONNAIRE; ?></label>
-            <input type="text" id="password" name="connex_mdp" placeholder="email adress">
+            <input type="text" name="new_email" placeholder="email adress">
 
             <label for="password"><?php echo _SAISIR_MDP; ?></label>
-            <input type="password" id="password" name="connex_mdp" placeholder="password">
+            <input type="password" name="verif_mdp" placeholder="password">
 
             <input type="submit" value="SUBMIT">
         </form>
@@ -19,13 +19,13 @@
         <h1 class="title_gest"><?php echo _SUP_GESTIONNAIRE; ?></h1>
         <form method="POST" action="">
             <label for="username"><?php echo _SAISIR_GEST; ?></label>
-            <input type="text" id="username" name="connex_login" placeholder="username">
+            <input type="text" name="sup_user" placeholder="username">
 
             <label for="password"><?php echo _SAISIR_GESTIONNAIRE; ?></label>
-            <input type="text" id="password" name="connex_mdp" placeholder="email adress">
+            <input type="text" name="sup_email" placeholder="email adress">
 
             <label for="password"><?php echo _SAISIR_MDP; ?></label>
-            <input type="password" id="password" name="connex_mdp" placeholder="password">
+            <input type="password" name="verif_mdp" placeholder="password">
 
             <input type="submit" value="SUBMIT">
         </form>
