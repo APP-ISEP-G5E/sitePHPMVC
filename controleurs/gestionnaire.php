812513d6050b954d15dca8e69b78b572d464ac07
Algonight <56296650+Algonight@users.noreply.github.com>
2019-12-02 11:33:08 +0100

    Add files via upload

--- controleurs/gestionnaire.php
+++ controleurs/gestionnaire.php
@@ -9,6 +9,8 @@
 /**
  * Contrôleur du gestionnaire
  */
+// on appelle le modèle qui fait appel aux requetes génériques
+include('./modele/requetes.gestionnaire.php');
 
 // si la fonction n'est pas définie, on choisit d'afficher l'accueil
 if (!isset($_GET['fonction']) || empty($_GET['fonction'])) {
@@ -26,6 +28,43 @@ switch ($function) {
         $css="CSSnav";
         $vue = "accueilGestionnaire";
         $title = "Accueil Gestionnaire";
+        break;
+
+    case 'alerteTemperature':
+        $title="Alertes des Capteurs de températures";
+        $vue="alerteTemperature";
+        $css="CSSalertes";
+        break;
+
+    case 'alerteCardiaque':
+        $title="Alertes des Capteurs cardiaques";
+        $vue="alerteCardiaque";
+        $css="CSSalertes";
+        break;
+
+    case 'alerteSonore':
+        $title="Alertes des Capteurs sonores";
+        $vue="alerteSonore";
+        $css="CSSalertes";
+        break;
+
+    case 'actionneurLumineux':
+        $title="Actionneur Lumineux";
+        $vue="actionneurLumineux";
+        $css="CSSactionneur";
+        break;
+
+    case 'actionneurSonore':
+        $title="Actionneur Sonore";
+        $vue="actionneurSonore";
+        $css="CSSactionneur";
+        break;
+
+    case 'donneesUtilisateursAnonymes' :
+        $title="Données des Utilisateurs Anonymes";
+        $donneesUtilisateurs=recupereDonneesUtilisateurs($bdd);
+        $vue="donnees_des_candidats_anonymes";
+        $css="CSSlisteUtilisateurs";
         break;
 
     default:
