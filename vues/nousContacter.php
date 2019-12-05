91a19b4d76295af58668cae20cd9f6f9ad4cd992
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-12-05 18:28:05 +0100

    Update nousContacter.php

--- vues/nousContacter.php
+++ vues/nousContacter.php
@@ -1,14 +1,30 @@
 <div id="vide"></div>
 <h1>Nous Contacter</h1>
+
 <div class="contact">
     <div id="mail">
         <img src="pictures/mail.png" height="100px" width="auto">
-        <p>admin@infinitemeasures.com</p>
+        <p>
+            <?php
+            foreach ($donneesNousContacter as $element) { ?>
+                <?php if($element['idFixe'] == 4){     //4 correspond à l'id mail
+                    echo $element['donneeFixe'];        //affiche le mail
+                } ?>
+            <?php } ?>
+        </p>
     </div>
 
     <div id="telephone">
         <img src="pictures/phone.png" height="100px" width="auto">
-        <p> +336 00 00 00 00</p>
+        <p>
+            <?php
+            foreach ($donneesNousContacter as $element) { ?>
+                <?php if($element['idFixe'] == 3){     //3 correspond à l'id téléphone
+                echo $element['donneeFixe'];          //affiche le numéro de téléphone
+                } ?>
+            <?php } ?>
+        </p>
+
     </div>
 
 </div>
