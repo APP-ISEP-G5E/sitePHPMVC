30600879b37fd6efa709562a0034b1baf5cda47c
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-12-05 18:27:25 +0100

    Add files via upload

---
+++ vues/mentionLegaleAdmin.php
@@ -0,0 +1,18 @@
+<?php
+require("mentionLegale.php");
+?>
+<div id="contenuML">
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="input">
+                <textarea name="contenuML" class="ML" rows="10" cols="120"><?php    //pour afficher le texte deja présent
+                    foreach ($donneesML as $element){
+                        if($element['idFixe'] ==2){     //2 correspond à l'id des Mentions Légales
+                            echo $element['donneeFixe']; }
+                    } ?>
+                </textarea>
+            </div>
+            <input type="submit" value="MODIFIER les Mentions légales">
+        </form>
+    </div>
+</div>
---
+++ vues/nousContacterAdmin.php
@@ -0,0 +1,29 @@
+<?php
+require("nousContacter.php");
+?>
+
+<div class="contact">
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="input">
+                <p>
+                <input type="email" name="contenuMail" placeholder="Modifier votre mail ici">
+                <input type="submit" value="MODIFIER l'adresse mail">
+                </p>
+            </div>
+        </form>
+    </div>
+
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="input">
+                <p>
+                    <input type="text" name="numeroTelephone" placeholder="Modifier le numéro de téléphone ici">
+                    <input type="submit" value="MODIFIER le numéro de téléphone">
+                </p>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div id="vide"> </div>
