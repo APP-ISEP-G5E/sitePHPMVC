d56ef3b18a108eab63a947bb7706fa46c2eb1b56
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-12-05 22:12:28 +0100

    Add files via upload

---
+++ vues/quisommesnousAdmin.php
@@ -0,0 +1,21 @@
+<?php
+require("accueil.php");
+?>
+<div id="contenuQSN">  <!--même mise en forme que les mentions légales-->
+    <div id="parametrer">
+        <form method="POST" action="">
+            <div class="input">
+                <textarea name="contenuQSN" class="ML" rows="10" cols="120"><?php    //pour afficher le texte deja présent
+                    foreach ($donneesQSN as $element){
+                        if($element['idFixe'] ==5){     //5 correspond à l'id de QSN
+                            echo $element['donneeFixe']; }
+                    } ?>
+                </textarea>
+            </div>
+            <input type="submit" value="MODIFIER le contenu de Qui sommes-nous">
+        </form>
+    </div>
+</div>
+
+<div id="Fplace"></div>
+<div id="Fplace"></div>
