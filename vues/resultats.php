4c0ae01c3aebec64ac929ed483eed29de36d6450
Algonight <56296650+Algonight@users.noreply.github.com>
2019-12-09 14:28:55 +0100

    Add files via upload

--- vues/resultats.php
+++ vues/resultats.php
@@ -2,19 +2,81 @@
 <h1>Liste des Utilisateurs inscrits</h1>
 <div id="contenuListeUttilisateurs">
     <div class="blocListeUtilisateurs">
-        <div class="blocId">
-            <p style="font-weight: bold">valeur</p>
+        <table id="myTable">
+            <tr >
+                <!--<th onclick="sortTable(0)">Id</th>-->
+                <th onclick="sortTable(1)">Valeur</i></th>
+                <th onclick="sortTable(2)">Date</th>
+                <th></th>
+            </tr>
             <?php
             foreach ($donneesMesure as $element) { ?>
-            <P><?php echo  $element['valeur']; ?></P>
+            <tr>
+                <!--<td><?php echo  $element['id']; ?></td>-->
+                <td><?php echo  $element['valeur']; ?></td>
+                <td><?php echo  $element['instant']; ?></td>
+                <td><span class="close">x</span></td>
+            </tr>
             <?php } ?>
-        </div>
-        <div class="blocNom">
-            <p style="font-weight: bold">Date</p>
-            <?php
-            foreach ($donneesMesure as $element) { ?>
-            <P><?php echo  $element['instant']; ?></P>
-            <?php } ?>
-        </div>
-    </div>  <!--bloc questionReponse-->
+        </table>
+
+    </div> <!--bloc questionReponse-->
 </div>
+
+<script>
+    function sortTable(n) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("myTable");
+        switching = true;
+        // Set the sorting direction to ascending:
+        dir = "asc";
+        /* Make a loop that will continue until
+        no switching has been done: */
+        while (switching) {
+            // Start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /* Loop through all table rows (except the
+            first, which contains table headers): */
+            for (i = 1; i < (rows.length - 1); i++) {
+                // Start by saying there should be no switching:
+                shouldSwitch = false;
+                /* Get the two elements you want to compare,
+                one from current row and one from the next: */
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                /* Check if the two rows should switch place,
+                based on the direction, asc or desc: */
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /* If a switch has been marked, make the switch
+                and mark that a switch has been done: */
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                // Each time a switch is done, increase this count by 1:
+                switchcount ++;
+            } else {
+                /* If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again. */
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
+</script>
