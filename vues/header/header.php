60b136dc9347a3160479ccfceb50b2fbdc7f66bd
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2020-01-22 14:24:38 +0100

    Add files via upload

--- vues/header/header.php
+++ vues/header/header.php
@@ -1,19 +1,19 @@
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="vues/<?php echo $css?>.css">
+    <link rel="stylesheet" type="text/css" href="vues/<?php echo $css ?>.css">
     <link rel="stylesheet" type="text/css" href="vues/header/CSSheader.css">
-    <link rel="stylesheet" type="text/css" href="vues/header/CSSnav.css">
+    <link rel="stylesheet" type="text/css" href="vues/accueil/CSSnav.css">
     <meta charset="utf-8">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
-        $( function() {
-            $( "#dialog" ).dialog();
-        } );
+        $(function () {
+            $("#dialog").dialog();
+        });
     </script>
-    <title><?php echo $title;?></title>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
 <header>
@@ -22,17 +22,31 @@
     <input type="checkbox" id="switch">
     <nav id="navheader" role="navigation">
         <ul>
-            <li><a class="MenuP" href="index.php?cible=utilisateurs&fonction=faq"><?php echo _FAQH; ?></a></li>
-            <li><a class="MenuP" href="index.php?"><?php echo _ACCUEIL; ?></a></li>
+            <li><a class="MenuP" href="index.php?cible=visiteur&fonction=faq"><?php echo _FAQH; ?></a></li>
+            <li><a class="MenuP" href="index.php"><?php echo _ACCUEIL; ?></a></li>
+            <?php if(isset($_COOKIE['connecter']) && $_COOKIE['connecter'] == "true"){?>
             <li><a class="MenuP" href="index.php?cible=utilisateurs&fonction=profil"><?php echo _MON_PROFIL; ?></a></li>
-            <li><a class="MenuP" href="index.php?cible=utilisateurs&fonction=connexion"><?php echo $_SESSION['connecter']; ?></a></li>
-            <li><a class="MenuP" href="index.php?cible=utilisateurs&fonction=langue"><img class="drapeau" src=<?php echo $drapeau;?>
-                                                                                          height="30" width="auto" alt="drapeau"></a></li>
+            <?php }?>
+            <li><a class="MenuP"
+                   href="index.php?cible=visiteur&fonction=connexion"><?php if(isset($_COOKIE['connecter']) && $_COOKIE['connecter']=="true"){
+                        echo _DECONNEXION;
+                    }else{
+                        echo _CONNEXION;
+                    } ?></a></li>
+            <li><a class="MenuP" href="index.php?cible=visiteur&fonction=langue"><img class="drapeau"
+                                                                                          src=<?php echo $drapeau; ?>
+                                                                                          height="30" width="auto"
+                                                                                          alt="drapeau"></a></li>
         </ul>
     </nav>
 </header>
-<?php if($alerte){  ?>
+<?php if ($alerte) { ?>
     <?php echo '<div id="dialog" title="Information">
-    <p>'.$alerte.'</p>
-</div>'; }?>
+    <p>' . $alerte . '</p>
+</div>';
+} ?>
+
+
+
+
 
